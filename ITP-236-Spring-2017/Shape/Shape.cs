a7946d36b0a7fddc25dc2e1e05c763fdf5999826
cljernigan <cljernigan85@gmail.com>
2017-01-18 21:34:07 -0500

    Add files via upload

---
+++ ITP-236-Spring-2017/Shape/Shape.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    public abstract class Shape
+    {
+        public abstract float Perimeter { get; }
+        public abstract float Area { get; }
+
+    }
+    public class Rectangle : Shape
+    {
+
+        public float Length { get; set; }
+        public float Width { get; set; }
+
+        public override float Perimeter
+        {
+            get { return (Length + Width) * 2; }
+        }
+
+        public override float Area
+        {
+            get { return Length * Width; }
+
+        }
+
+        public Rectangle() : this(1, 1)
+        { }
+
+        public Rectangle(float length, float width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+
+
+    }
+
+    public class Circle : Shape
+    {
+        public float Radius { get; set; }
+        public override float Perimeter
+        {
+            get { return 2*(float) Math.PI*Radius; }
+        }
+
+        public override float Area
+        {
+            get { return (float) Math.PI*Radius*Radius;}
+        }
+
+        public Circle() : this(1)
+        {
+        }
+
+        public Circle(float radius)
+        {
+            Radius = radius;
+        }
+    }
+
+
+}
+
+
---
+++ ITP-236-Spring-2017/ShapeXface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shape;
+
+namespace ShapeXface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Circle circle = new Circle();
+            Display(circle);
+            Display(rectangle);
+            Console.ReadLine();
+        }
+
+        static void Display(Shape.Shape shape)
+        {
+            Console.WriteLine(string.Format("Perimiter: {0}. Area: {1}",
+                shape.Perimeter, shape.Area));
+        }
+    }
+}
