5b97ddd7197b79b2bf9c5fe9eaff1a6e0d89a3f6
cljernigan <cljernigan85@gmail.com>
2017-02-08 19:55:36 -0500

    Add files via upload

---
+++ ITP-236-Spring-2017/Extension/Extension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class Extension
+    {
+        public static string Left(this string name, int length)
+        {
+            if (name == null)
+                return name;
+            return length > name.Length ? name : name.Substring(0, length);
+        }
+
+        public static string Right(this string name, int length)
+        {
+            if (name == null)
+                return name;
+           
+            return length > name.Length ? name : name.Substring(name.Length - length, length) ;
+        }
+
+    }
+}
---
+++ ITP-236-Spring-2017/LinqConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsole
+{
+    class Program
+    {
+
+        delegate int Numbers(int[] numbers);
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 123, 12, 12, 13, 51, 35, 82, 816, 36 };
+            Numbers Happy = SumTheNumbers;
+            int total = Happy(numbers);
+            Happy = MaximumNumber;
+            int max = Happy(numbers);
+            Console.Write($"HAPPY {total}th Birthday");
+            Console.Write($"Low is {max}");
+            Console.ReadLine();
+        }
+        static int MaximumNumber(int[] numbers)
+        {
+            int max = int.MinValue;
+            foreach (var num in numbers)
+                if (num > max)
+                    max = num;
+            return max;
+        }
+
+
+        static int SumTheNumbers(int[] numbers)
+        {
+            int total = 0;
+            foreach (int num in numbers)
+                total += num;
+            return total;
+        }
+
+    }
+}
